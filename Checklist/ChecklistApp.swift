7cbf7e4d0d84232e2417f6f3f78ad74f9d45bc0e
aybjax <aybat93@gmail.com>
2021-05-25 22:15:51 +0600

    Initial Commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by aybjax on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
